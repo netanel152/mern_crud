c8c14e08971bf15c7681c53631da2eb7ff1ec882
netanel152 <netaenlos152@gmail.com>
2022-07-30 23:01:42 +0300

    connet login and register to redux toolkit and fix some ui bug

--- fronted/src/pages/Dashboard.jsx
+++ fronted/src/pages/Dashboard.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect, useDeferredValue } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate("/");
+    }
+    if (!user) {
+      navigate("/login");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return <div>Dashboard of {user?.name}</div>;
 }
 
 export default Dashboard;
